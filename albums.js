57a67d025c0c41ea3a3e5a7ad492c601e8c2bf14
jwandr <j3gray@gmail.com>
2026-04-11 08:46:07 +0800

    Albums

--- albums.js
+++ albums.js
@@ -2,35 +2,43 @@
 
 const albums = [
   {
-    album: "OK Computer",
-    artist: "Radiohead",
-    year: 1997,
-    tags: ["alternative", "rock", "classic"],
-    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
-    spotify: "https://open.spotify.com/album/4xWXI3gVZ8T7d1xQ7Z5Q5J"
-  },
-  {
-    album: "Random Access Memories",
-    artist: "Daft Punk",
-    year: 2013,
-    tags: ["electronic", "funk", "nostalgic"],
-    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
-    spotify: "https://open.spotify.com/album/4m2880jivSbbyEGAKfITCa"
-  },
-  {
-    album: "In Rainbows",
-    artist: "Radiohead",
-    year: 2007,
-    tags: ["alternative", "experimental", "dreamy"],
-    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
-    spotify: "https://open.spotify.com/album/5vkqYmi5m8fG8v7y7f7f7f"
-  },
-  {
     album: "Jollification",
     artist: "The Lightning Seeds",
     year: 1994,
-    tags: ["Jeff","Things"],
+    tags: ["Jeff","things"],
     image: "https://image-cdn-ak.spotifycdn.com/image/ab67616d00001e02c14763d9328c629961e15dcb",
     spotify: "https://open.spotify.com/album/2aPTNYJ6MjDj7UvIHnJs26?si=qJvDSnBPTDia1_7FJ8nrPg"
-  }
+  },
+{
+    album: "In Rainbows",
+    artist: "Radiohead",
+    year: 2007,
+    tags: ["something"],
+    image: "https://image-cdn-fa.spotifycdn.com/image/ab67616d00001e02de3c04b5fc750b68899b20a9",
+    spotify: "https://open.spotify.com/album/5vkqYmiPBYLaalcmjujWxK?si=8affc7f058454a6c"
+  },
+{
+    album: "O3",
+    artist: "Sunscreen",
+    year: 1993,
+    tags: ["indie"],
+    image: "https://image-cdn-ak.spotifycdn.com/image/ab67616d00001e02eb3f546acb3f78e9d0f6dd13",
+    spotify: "https://open.spotify.com/album/74rtjNB8yXAi8HcTek9C3G?si=-_5TxT91RbmsvNfw0l1MLA"
+  },
+{
+    album: "A Head Full of Dreams",
+    artist: "Coldplay",
+    year: 2015,
+    tags: ["indie"],
+    image: "https://image-cdn-fa.spotifycdn.com/image/ab67616d00001e028ff7c3580d429c8212b9a3b6",
+    spotify: "https://open.spotify.com/album/3cfAM8b8KqJRoIzt3zLKqw?si=bDO0OYtuRiWMI8BYBM9D_Q"
+  },
+{
+    album: "Ghost Stories",
+    artist: "Coldplay",
+    year: 2014,
+    tags: ["indie"],
+    image: "https://image-cdn-ak.spotifycdn.com/image/ab67616d00001e02e5a95573f1b91234630fd2cf",
+    spotify: "https://open.spotify.com/album/2G4AUqfwxcV1UdQjm2ouYr?si=vo-cx20_SC2kgXf6rSuKxg"
+  },
 ];
