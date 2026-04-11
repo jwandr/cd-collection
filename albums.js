839cb2627fcf71b3fa3b179b5a36edcc5b4ae044
jwandr <j3gray@gmail.com>
2026-04-11 08:08:37 +0800

    Split out album data

---
+++ albums.js
@@ -0,0 +1,28 @@
+// albums.js
+
+const albums = [
+  {
+    album: "OK Computer",
+    artist: "Radiohead",
+    year: 1997,
+    tags: ["alternative", "rock", "classic"],
+    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
+    spotify: "https://open.spotify.com/album/4xWXI3gVZ8T7d1xQ7Z5Q5J"
+  },
+  {
+    album: "Random Access Memories",
+    artist: "Daft Punk",
+    year: 2013,
+    tags: ["electronic", "funk", "nostalgic"],
+    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
+    spotify: "https://open.spotify.com/album/4m2880jivSbbyEGAKfITCa"
+  },
+  {
+    album: "In Rainbows",
+    artist: "Radiohead",
+    year: 2007,
+    tags: ["alternative", "experimental", "dreamy"],
+    image: "https://i.scdn.co/image/ab67616d0000b273c1b6a2a2f9f4d3f1c8f5c1a2",
+    spotify: "https://open.spotify.com/album/5vkqYmi5m8fG8v7y7f7f7f"
+  }
+];
